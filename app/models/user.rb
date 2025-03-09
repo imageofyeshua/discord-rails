9483341e22544395a0621e3934d1c26161bd42cd
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-09 19:35:35 +0900

    User Number Logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+class User < ApplicationRecord # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :set_user_number, if: -> { !user_number.present? }
+
+  private
+
+  def set_user_number
+    other_users_with_same_username = User.where(username: username)
+    self.user_number = other_users_with_same_username.count + 1
+  end
 end
